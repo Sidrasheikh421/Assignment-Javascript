5c04c9610fe0b5be1f180872dfe315a7ae2b6918
Madihakhan612 <Madihakhanpk126@gmail.com>
2026-08-16 21:56:56 +0500

    done

---
+++ Chapter no 14-16/app.js
@@ -0,0 +1,29 @@
+//Question 1
+
+var studentName =["Ayesha","Mehak","Zoya","Hafsa"];
+console.log(studentName[0]);
+
+//Question 2
+
+var studentName =new Array("Sehar","Noor","Sarah","Fatima");
+console.log(studentName[2]);
+
+//Question 3
+
+var fruitsName =["Mango","Banana","apple","Orange"];
+console.log(fruitsName);
+
+//Question 4
+
+var numbers =[10,20,30,40,50];
+console.log(numbers);
+
+//Question 5
+
+var booleanValue =[true,false];
+console.log(booleanValue);
+
+//Question 6
+
+var mixedArray =["Fatima",10,"Maryam",true,30];
+console.log(mixedArray);
