4003b881d2c75ad516a8bb39d52fea121ee6dcb5
Sidrasheikh421 <sidrasheikhpk30@gmail.com>
2026-09-04 13:32:36 +0500

    Completed

---
+++ Chapter no 26 -30/app.js
@@ -0,0 +1,87 @@
+//Question 1
+
+var number = 3.45214;
+number = parseFloat(number);
+
+document.write("Number: " + number + "<br>");
+document.write("Round off value: "   + Math.round(number) + "<br>");
+document.write("Floor value: "  + Math.floor(number) + "<br>");
+document.write("Ceil value: "   + Math.ceil(number) + "<br>");
+document.write("<br/>");
+
+//Question 2
+
+var number = -2.673;
+number = parseFloat(number);
+
+document.write("Number: " + number + "<br>");
+document.write("Round off value: "   + Math.round(number) + "<br>");
+document.write("Floor value: "  + Math.floor(number) + "<br>");
+document.write("Ceil value: "   + Math.ceil(number) + "<br>");
+document.write("<br/>");
+
+//Question 3
+
+var number = -4;
+number = parseFloat(number);
+var absoluteValue = Math.abs(number);
+document.write("The absolute value of " + number + " is " + absoluteValue + "<br>");
+document.write("<br/>");
+
+//Question 4
+
+var dice = Math.floor(Math.random() * 6) + 1;
+document.write("Random dice value: " + dice + "<br>");
+document.write("<br/>");
+
+//Question 5
+var coin1 = Math.floor(Math.random()* 2) + 1;
+var coin2 = Math.floor(Math.random() * 2) + 1;
+
+if (coin1 === 1) {
+    document.write("Random coin value: Tails<br>");
+
+}
+else {
+    document.write("Random coin value: Heads<br>");
+
+}
+document.write("<br/>");
+if (coin2 === 1) {
+    document.write("Random coin value: Heads<br>");
+
+}
+else {
+    document.write("Random coin value: Tails<br>");
+
+}
+document.write("<br/>");
+
+
+//Question 6
+
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+ document.write("Random number between 1 and 100: " + randomNumber + "<br>");
+document.write("<br/>");
+
+//Question 7
+
+var weight = prompt("Enter your weight in kilograms:");
+ var userweight = parseFloat(weight);
+ document.write("The weight of user is " + userweight + " kilograms<br>");
+document.write("<br/>");
+
+//Question 8 
+
+var secretNumber = prompt("Enter a number between 1 and 10:");
+
+var userGuess = parseInt(secretNumber);
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+
+if (userGuess === randomNumber){
+    document.write("Congratulations! You guessed the correct number: " + randomNumber + "<br>");
+}
+else{
+    document.write("Sorry, the correct number was: " + randomNumber + ". Better luck next time!<br>");
+}
